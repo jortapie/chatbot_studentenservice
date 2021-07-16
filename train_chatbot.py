6bb20c964bf27056be285602060a63d256f69496
jtapiac <jorge.tapia@ferreyros.com.pe>
2021-07-16 10:41:06 -0500

    Avance de primer video acabado

--- train_chatbot.py
+++ train_chatbot.py
@@ -1,5 +1,5 @@
 import nltk
-nltk.download()
+# nltk.download()
 import json
 # import pickle
 
@@ -11,9 +11,22 @@ data_file = open('intents.json').read()
 intents = json.loads(data_file)
 
 for intent in intents['intents']:
+    # print(intents['intents'])
+    # TODO: Corregir repeticion de los intent.
     for pattern in intent['patterns']:
+        # print(intent['patterns'])
         # tokenize here
         w = nltk.word_tokenize(pattern)
-        print('Token is: '.format(w))
+        print('Token is: {}'.format(w))
         words.extend(w)
         documents.append((w, intent['tag']))
+        # add the tag to classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+    # Final lists
+    print('Words list is: {}'.format(words))
+    print('Docs are: {}'.format(documents))
+    print('Classes are: {}'.format(classes))
+
+print('Eeeeexito')
